6bf89c9ab5cf59ba1d2ba04295ac73de8186fe2f
Jakub Krymarys <krymarys.jakub@gmail.com>
2021-07-03 14:39:40 +0200

    Add temp graph based on dummy data

---
+++ src/utils/dummyData.ts
@@ -0,0 +1,14 @@
+export const dummyTempData = [
+  { dateTime: "2021-05-21T09:00:50", temp: 19 },
+  { dateTime: "2021-05-21T10:00:50", temp: 20 },
+  { dateTime: "2021-05-21T11:00:50", temp: 20.1 },
+  { dateTime: "2021-05-21T12:00:50", temp: 20.3 },
+  { dateTime: "2021-05-21T13:00:50", temp: 20.4 },
+  { dateTime: "2021-05-21T14:00:50", temp: 20.8 },
+  { dateTime: "2021-05-21T15:00:50", temp: 20.3 },
+  { dateTime: "2021-05-21T16:00:50", temp: 20.1 },
+  { dateTime: "2021-05-21T17:00:50", temp: 19.3 },
+  { dateTime: "2021-05-21T18:00:50", temp: 18.0 },
+  { dateTime: "2021-05-21T19:00:50", temp: 17.05 },
+  { dateTime: "2021-05-21T20:00:50", temp: 17 },
+];
