a0e482e0ab715e7a7bdfa779e6cc431c852ac60b
Jakub Krymarys <krymarys.jakub@gmail.com>
2021-08-20 17:17:15 +0200

    Add Button component

---
+++ src/components/controls/Button.tsx
@@ -0,0 +1,9 @@
+function Button() {
+  return (
+    <button className="bg-white hover:bg-gray-300 border border-gray-500 hover:border-transparent text-black font-semibold py-2 px-4 rounded">
+      Test
+    </button>
+  );
+}
+
+export default Button;
