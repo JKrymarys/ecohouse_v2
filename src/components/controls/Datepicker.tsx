6d168ed308c87b728ea7f89fdaf1a7b01730e3c7
Jakub Krymarys <krymarys.jakub@gmail.com>
2021-08-20 17:17:30 +0200

    Add datepicker component

---
+++ src/components/controls/Datepicker.tsx
@@ -0,0 +1,10 @@
+function Datepicker() {
+  return (
+    <input
+      type="date"
+      className="bg-white hover:bg-gray-300 border border-gray-500 hover:border-transparent text-black py-2 px-4 rounded"
+    />
+  );
+}
+
+export default Datepicker;
